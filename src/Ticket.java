d545426db0b39e030cff0d5122e5fccaadb1dcd8
VladimirSKLV <vladimirsokolov969@gmail.com>
2022-07-06 12:45:49 +0300

    v2

---
+++ src/Ticket.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Ticket {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+
+        int costIsRight = service.calculate(10000);
+        System.out.println(costIsRight);
+
+        System.out.println();
+        int costIsInvalid = service.calculate(-10000);
+        System.out.println(costIsInvalid);
+
+        System.out.println();
+        int costIsOne = service.calculate(1);
+        System.out.println(costIsOne);
+
+        System.out.println();
+        int costIsTwenty = service.calculate(20);
+        System.out.println(costIsTwenty);
+    }
+}
